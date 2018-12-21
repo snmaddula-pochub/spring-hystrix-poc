c2745b921aef0ac5755d76a78ef90531ea4fcfc7
snmaddula <snmaddula@outlook.com>
2018-12-22 04:40:53 +0530

    Added dashboard support

--- src/main/java/snmaddula/pochub/hystrix/SpringHystrixPocApplication.java
+++ src/main/java/snmaddula/pochub/hystrix/SpringHystrixPocApplication.java
@@ -2,12 +2,14 @@ package snmaddula.pochub.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableCircuitBreaker
+@EnableHystrix
 @SpringBootApplication
+@EnableHystrixDashboard
 public class SpringHystrixPocApplication {
 
 	public static void main(String[] args) {
